9000bce0580f951b00a4415aceabe97c81ce561e
mooserew <pedrodvdmf@gmail.com>
2023-09-23 21:00:08 +0100

    First commit, working on main.py message showing

--- exc/main.py
+++ exc/main.py
@@ -1,7 +1,10 @@
 import pyfiglet
 import sys
-from portscanner import scanny
-from wifiscanner import scannywi
 
-ascii_header = pyfiglet.figlet_format("PREPY")
+ascii_header = pyfiglet.figlet_format("Loggy")
 print(ascii_header)
+ 
+inp = input(str("Welcome to Loggy, to show a list of available commands type -help: "))
+
+if inp == "-help":
+    print("-portscan: look for open ports\n-wifiscan: scan every device connected to your network")
